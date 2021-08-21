963c6026dc40e1115b76d6809c4f1d8ca3c7174a
Adrian Struszczyk <adrianstru@gmail.com>
2021-08-21 11:49:09 -0400

    Replace scrollreveal

--- src/components/ScrollReveal/ScrollReveal.js
+++ src/components/ScrollReveal/ScrollReveal.js
@@ -1,21 +1,11 @@
-import React, { useRef, useEffect } from "react";
-import scrollReveal from "scrollreveal";
+import React from "react";
 
 export const ScrollReveal = ({ children }) => {
-  const sectionRef = useRef();
-  useEffect(() => {
-    if (typeof(window) !== 'undefined' && sectionRef.current)
-      scrollReveal().reveal(sectionRef.current, {
-        reset: false,
-        delay: 50,
-        distance: '50px',
-        origin: 'bottom'
-      });
-  }, []);
-
   return (
     <div
-      ref={sectionRef}
+      data-sal="slide-up"
+      data-sal-delay="50"
+      data-sal-easing="ease"
       className="media"
     >
       {children}
