88a6521351f521f79412274de9a711d9be62d074
Adrian Struszczyk <adrianstru@gmail.com>
2021-08-21 14:08:23 -0400

    Add emotion to gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,6 +69,17 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-emotion`,
+      options: {
+        // Accepts the following options, all of which are defined by `@emotion/babel-plugin` plugin.
+        // The values for each key in this example are the defaults the plugin uses.
+        sourceMap: true,
+        autoLabel: "dev-only",
+        labelFormat: `[local]`,
+        cssPropOptimization: true,
+      },
+    },
+    {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
