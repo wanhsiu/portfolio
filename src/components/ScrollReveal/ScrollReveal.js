43202823e1e973ec7e714233d879106407bcfc1e
Adrian Struszczyk <adrianstru@gmail.com>
2021-08-22 20:07:12 -0400

    .

--- src/components/ScrollReveal/ScrollReveal.js
+++ src/components/ScrollReveal/ScrollReveal.js
@@ -3,9 +3,6 @@ import React from "react";
 export const ScrollReveal = ({ children }) => {
   return (
     <div
-      data-sal="slide-up"
-      data-sal-delay="50"
-      data-sal-easing="ease"
       className="media"
     >
       {children}
