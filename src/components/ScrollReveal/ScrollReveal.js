5173ced1e77b2f8fe4a7c246197b67178193bd85
Adrian Struszczyk <adrianstru@gmail.com>
2021-09-19 09:08:14 -0400

    Revert "Re-add scrollreveal."
    This reverts commit 89bcd819fc9c18c512ad13ab16131083846afaa8.

--- src/components/ScrollReveal/ScrollReveal.js
+++ src/components/ScrollReveal/ScrollReveal.js
@@ -1,21 +1,8 @@
-import React, { useRef, useEffect } from "react";
-import scrollReveal from "scrollreveal";
+import React from "react";
 
 export const ScrollReveal = ({ children }) => {
-  const sectionRef = useRef();
-  useEffect(() => {
-    if (typeof(window) !== 'undefined' && sectionRef.current)
-      scrollReveal().reveal(sectionRef.current, {
-        reset: false,
-        delay: 50,
-        distance: '50px',
-        origin: 'bottom'
-      });
-  }, []);
-
   return (
     <div
-      ref={sectionRef}
       className="media"
     >
       {children}
